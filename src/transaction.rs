1fe5b44d6f96ca769b51de3f27b327cdaa7366c5
hank-butler <hank.butler@protonmail.com>
2024-09-20 09:35:48 -0700

    update to transaction, wallet, added rand crate

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,4 +1,6 @@
 
+
+
 pub const TRANSACTION_FEE: f64 = 1.0;
 
 pub enum TransactionType {
@@ -28,7 +30,7 @@ impl TransactionInput {
 pub struct Transaction {
     // need to build out this struct
     pub id: usize,
-    pub transaction_type: TransactionType, // need to make traits?,
+    pub transaction_type: TransactionType, 
     pub transaction_input: TransactionInput,
     pub transaction_output: TransactionOutput,
     // sender, receiver, timestamp, amount
--- src/wallet.rs
+++ src/wallet.rs
@@ -1,7 +1,12 @@
+use serde::{Deserialize, Serialize};
+use rand::rngs::OsRng;
+
 pub struct Wallet {
     pub key_pair: String,
 }
 
 impl Wallet {
-    
+    pub fn new() -> Wallet {
+
+    }
 }
