e9653ce70373fc1433cf01c4072e3fa002ff1e02
hank-butler <hank.butler@protonmail.com>
2024-09-24 23:25:46 -0600

    logic for wallet impl

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,4 +1,6 @@
 use serde::{Deserialize, Serialize};
+use rand::rngs::OsRng;
+use ed25519_dalek::{Keypair, Signer}; // helps with keypair generation
 
 
 pub struct Wallet {
@@ -7,6 +9,17 @@ pub struct Wallet {
 
 impl Wallet {
     pub fn new() -> Wallet {
+        let mut csprng = OsRng {}; // need to generate psuedo-random numbers for keypairs
+        let keypair = Keypair::generate(&mut csprng);
 
+        let public_key = hex::encode(keypair.public.to_bytes());
+
+        println!("Public Key: {}", public_key);
+
+        let keypair = hex::encode(keypair.to_bytes());
+
+        println!("Keypair: {}", keypair);
+
+        Self { keypair }
     }
 }
