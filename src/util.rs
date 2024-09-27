db784142bba00a32637e082cfd54a19b59eb1fd2
hank-butler <hank.butler@protonmail.com>
2024-09-26 21:33:38 -0600

    hash function in util with Sha2::Sha256

--- src/util.rs
+++ src/util.rs
@@ -22,5 +22,30 @@ impl Util {
         Uuid::new_v4()
     }
 
-    // pub f
+    pub fn verify_signature(
+        from_public_key: &String,
+        message: &String,
+        from_signature: &String,
+    ) -> Result<bool, VerifySigErr> {
+        let public_key = hex::decode(from_public_key);
+        let dalek_public_key = PublicKey::from_bytes(&public_key)?;
+
+        let signature = hex::decode(from_signature)?;
+        let dalek_sig = &Signature::from_bytes(&signature)?;
+
+        Ok(dalek_public_key
+            .verify(message.as_bytes(), dalek_sig)
+            .is_ok())
+    }
+
+    pub fn hash(data: &String) -> String {
+        
+        let mut hasher = Sha256::default();
+
+        hasher.input(data);
+
+        let output = hasher.result().to_string();
+
+        output
+    }
 }
