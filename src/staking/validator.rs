261974fd0a18d553c4e634b43c7a5eca9779b894
hank-butler <hank.butler@protonmail.com>
2025-01-09 10:37:42 -0700

    refactoring of validator file, restart main

--- src/staking/validator.rs
+++ src/staking/validator.rs
@@ -1,8 +1,9 @@
 use serde::{Serialize, Deserialize};
+use crate::blockchain::blockchain::Blockchain;
 // use serde_json;
 use crate::util::{hash_input, chrono_timestamp,generate_random_number};
 use crate::blockchain::block::Block;
-// use crate::blockchain::blockchain::Blockchain;
+use crate::mempool::{CandidateStore, Storage};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 
@@ -24,7 +25,10 @@ pub fn get_validator_weight(stake: u64, total_votes: u64) -> u128 {
     return weight;
 }
 
-
+pub fn get_candidate_pool(storage: &Storage, height: u64) -> Blockchain {
+    let serialized_pool = CandidateStore::height(storage, height.clone()).unwrap().expect("Failed to get mempool");
+    Blockchain::from_string(serialized_pool)
+}
 
 
 // use crate::transaction::Transaction;
